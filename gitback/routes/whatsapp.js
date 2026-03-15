866163e73a80ad965fc1e6d1fac8116c566d0b2c
Adapa abhishek <abhiadapa0709@gmail.com>
2026-03-15 18:47:50 +0530

    Update whatsapp.js

--- gitback/routes/whatsapp.js
+++ gitback/routes/whatsapp.js
@@ -9,7 +9,7 @@ const {phone,message} = req.body
 
 await axios.post(
 
-`https://graph.facebook.com/v18.0/${process.env.WHATSAPP_PHONE_ID}/messages`,
+`https://graph.facebook.com/v18.0/${process.env.https://wa.me/qr/EICSQ2PLCYW7K1}/messages`,
 
 {
 messaging_product:"whatsapp",
